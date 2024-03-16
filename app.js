ee97ff3059a4a7f36d2e95f3db0f4c76237850da
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2024-03-16 13:11:40 +0500

    File Added

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+const { error } = require("console");
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from the server");
+  // We will read file over here!
+
+  fs.readFile("./data.txt", "utf-8", (err, data) => {
+    if (err) res.send("There was an error accessing the file");
+    res.send(data);
+  });
+
+  // res.send("Hello from the server");
 });
 
-app.get("/cars", (req, res) => {
-  res.send("This is for cars");
+app.get("/write", (req, res) => {
+  fs.appendFile("./data.txt", "\nCherry", (err) => {
+    if (err) res.send("Error Writin to File!");
+    res.send("Data Written Successfully");
+  });
+
+  // res.send("This is for fruits")
 });
 
 app.get("/fruits", (req, res) => {
